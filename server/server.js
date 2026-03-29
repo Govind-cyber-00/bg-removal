ff29dc908ad73c7e7cb034b6e4e4dd62cd1d87de
Govind-cyber-00 <govindyadav46324@gmail.com>
2026-03-29 17:36:02 +0530

    fix vercel express server

--- server/server.js
+++ server/server.js
@@ -3,17 +3,18 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 
-// ++++App Config ++++
-
-const PORT = process.env.PORT || 4000
+// App Config
 const app = express()
 
+// Connect DB
 await connectDB()
 
-// Initialize Middlewares
+// Middlewares
 app.use(express.json())
 app.use(cors())
 
-// API Routes
-app.get('/',(req,res) => res.send("API Working"))
-app.listen(PORT, () =>console.log("Server Running On port" +PORT))
+// Routes
+app.get('/', (req, res) => res.send("API Working"))
+app.get('/api/test', (req, res) => res.json({ success: true, message: "API is working" }))
+
+export default app
